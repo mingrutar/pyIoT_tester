757ff2d9a92a553fef9126aef0875c46a53f77f4
ming <rutar.ming@gmail.com>
2019-11-02 02:04:22 -0700

    change name

--- demo.sh
+++ demo.sh
@@ -3,4 +3,4 @@ set -e
 
 # run pub/sub sample app using certificates downloaded in package
 printf "\nRunning pub/sub sample application...\n"
-python device_sim.py -d pysimulato15 -e a3a13fa4bs6nw5-ats.iot.us-west-2.amazonaws.com -r ./root-CA.crt -c ./IoTTestDevice.cert.pem -k ./IoTTestDevice.private.key
+python device_sim.py -d pyandroid15 -e a3a13fa4bs6nw5-ats.iot.us-west-2.amazonaws.com -r ./root-CA.crt -c ./IoTTestDevice.cert.pem -k ./IoTTestDevice.private.key
